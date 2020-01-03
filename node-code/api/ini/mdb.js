ffb8801cdfedc75baea1cc7946268392315c0e0b
kdd family <54230553+kddfam@users.noreply.github.com>
2020-01-03 11:51:53 +0530

    Create mdb.js

---
+++ node-code/api/ini/mdb.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const success = require('debug')('success');
+const failed = require('debug')('failed');
+const chalk = require('chalk');
+
+module.exports = function () {
+    mongoose.connect('mongodb://localhost/authentication',
+        {
+            useCreateIndex : true,
+            useFindAndModify : true,
+            useNewUrlParser : true,
+            useUnifiedTopology : true
+        }).then(suc => success(chalk.cyanBright.bold('Connected to MongoDB')))
+          .catch(error => failed(chalk.redBright.bold(error)))
+};
