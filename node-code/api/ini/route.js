26d9a0d54b5c9d3e9204329b9ef8a67b832ebe84
kdd family <54230553+kddfam@users.noreply.github.com>
2020-01-03 15:49:00 +0530

    Update route.js

--- node-code/api/ini/route.js
+++ node-code/api/ini/route.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const Register = require('../routes/Register.js');
 const Login = require('../routes/Login.js');
+const ForgotPassword = require('../routes/ForgotPassword.js');
+const VerifyOTP = require('../routes/VerifyOTP.js');
 
 module.exports = function (app) {
     app.use(express.json());
     app.use(express.urlencoded({extended : true}));
-    app.use('/api/register', Register);
+    app.use('/api/register', Register)
     app.use('/api/login', Login);
+    app.use('/api/fp', ForgotPassword);
+    app.use('/api/vo', VerifyOTP);
 };
