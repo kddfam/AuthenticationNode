e13627dae971ecc6a7c3b617055729eefafd0441
kdd family <54230553+kddfam@users.noreply.github.com>
2020-01-03 13:26:22 +0530

    Update route.js

--- node-code/api/ini/route.js
+++ node-code/api/ini/route.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const Register = require('../routes/Register.js');
+const Login = require('../routes/Login.js');
 
 module.exports = function (app) {
     app.use(express.json());
     app.use(express.urlencoded({extended : true}));
-    app.use('/api/register', Register)
+    app.use('/api/register', Register);
+    app.use('/api/login', Login);
 };
