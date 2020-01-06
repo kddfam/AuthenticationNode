bab0f55c14ca761330e6ecec602be66addf018ab
kdd family <54230553+kddfam@users.noreply.github.com>
2020-01-06 10:00:38 +0530

    Comments added

--- node-code/authentication.js
+++ node-code/authentication.js
@@ -5,9 +5,24 @@ const failed = require('debug')('failed');
 const chalk = require('chalk');
 const config = require('config');
 
-if(!config.get("pg_user") && !config.get("pg_password") && !config.get("pg_database")) {
-    failed(chalk.blueBright.bold('Postgres Infomation Missing'));
+if(!config.get("pg_user")) {
+    failed(chalk.blueBright.bold('Postgres User Missing'));
     process.exit(1);
+};
+
+if(!config.get("pg_password")) {
+    failed(chalk.blueBright.bold('Postgres Password Missing'));
+    process.exit(1);
+}
+
+if(!config.get("pg_database")) {
+    failed(chalk.blueBright.bold('Postgres Database Missing'));
+    process.exit(1);
+}
+
+if(!config.get("jwtKey")) {
+    failed(chalk.blueBright.bold('JWT Key Missing'))
+    process.exit(1)
 };
 
 require('./api/ini/mdb.js')();
