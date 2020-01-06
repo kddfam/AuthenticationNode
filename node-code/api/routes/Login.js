884602a729f63cac106532f24182a00c8e045019
kdd family <54230553+kddfam@users.noreply.github.com>
2020-01-06 09:56:08 +0530

    Comments added

--- node-code/api/routes/Login.js
+++ node-code/api/routes/Login.js
@@ -20,49 +20,80 @@ router.post('/', async(req,res) => {
         password : config.get("pg_password"),
         database : config.get("pg_database")
     });
+
+    // connecting to postgres
     pool.connect()
         .then(success(chalk.cyanBright.bold('Connected for Login route.')))
         .catch(err => failed(chalk.redBright.bold(err)));
 
+    // validating values entered by user
     const {validation_error} = validateInputs(req.body);
     if(validation_error) {
+
+        // log error in file
         error.log({
             level : 'error',
             message : error.details[0].message,
             time : mongoose.Types.ObjectId().getTimestamp()
         });
+
+        // display error message on console
         failed(chalk.red.bold(error.details[0].message)+chalk.blue.bold(mongoose.Types.ObjectId().getTimestamp()));
+
+        // return response to user
         return res.status(500).json({
             isSuccessful : false,
             message : error.details[0].message
         });
     }   
     
+    // getting values from request's body
     const phonenumber = req.body.phonenumber;
     const password = req.body.password;
 
-    const checkPhoneNumber = `SELECT * FROM users WHERE phonenumber = $1 AND password = $2`
-    const values = [phonenumber,password]
+    // database query
+    const checkPhoneNumberQuery = `SELECT * FROM users WHERE phonenumber = $1 AND password = $2`
+    const checkPhoneNumberValues = [phonenumber,password]
 
-    pool.query(checkPhoneNumber,values)
+    // firing database query
+    pool.query(checkPhoneNumberQuery,checkPhoneNumberValues)
         .then(result => {
+            // checking whether phone number exists or not
+
+            // if not exists
             if(result.rowCount == 0) {
+
+                // display message on console
                 failed(chalk.red.bold(`${phonenumber} Not Found`))
+
+                // return response to user
                 return res.status(400).json({
                     isSuccessful : false,
                     message : `${phonenumber} Not Found`
                 });
             }
+
+            // if exists
             else {
+
+                // matching password
                 if(result.rows[0].password == password) {
+
+                    // if match, display success message on console
                     success(chalk.green.bold('Welcome Back'))
+
+                    // return response to user
                     return res.status(200).json({
                         isSuccessful : true,
                         message : `Welcome ${result.rows[0].firstname}`
                     });
                 }
                 else {
+
+                    // if dont match, display error message on console
                     failed(chalk.red.bold('Password Does not match'))
+
+                    // return response to user
                     return res.status(400).json({
                         isSuccessful : false,
                         message : 'Password Does not match'
@@ -71,12 +102,17 @@ router.post('/', async(req,res) => {
             }
         })
         .catch(err => {
+
+            // display error on console
             failed(chalk.red.bold(err));
+
+            // return response to user
             return res.status(400).json(err);
         })
     
 })
 
+// function for validating user input values
 function validateInputs(data) {
     const schema = joi.object({
         phonenumber : joi.number().required(),
